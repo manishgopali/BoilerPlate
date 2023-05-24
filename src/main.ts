00cadb88894d4cd2becee2bfeae5359dd3273194
Manish <manishgopali123@gmail.com>
2023-05-24 17:55:18 +0545

    firstcomit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+// import express from 'express'
+
+// // console.log("hello world updated");
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+
+// const add = (a:number,b:number) :number => a+b;
+
+// console.log(add(2,2));
+
+
+
+
+
